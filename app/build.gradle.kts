2e8dfedd03926bbf5a4c86bc6081404e9d05d140
moonbai <moonbai@users.noreply.github.com>
2026-05-31 12:31:17 +0000

    添加 Compose Compiler 插件以支持 Kotlin 2.0 - 在根 build.gradle.kts 中添加 org.jetbrains.kotlin.plugin.compose 插件 - 在 app/build.gradle.kts 中应用该插件 - 移除旧的 composeOptions 配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -61,9 +62,7 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/AL2.0"
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ buildscript {
 
 plugins {
     id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
 
 allprojects {
